b855bb4df0891611e22fcf0c3f80936eb5583691
saattrupdan <saattrupdan@gmail.com>
2024-02-05 17:19:45 +0100

    feat: Table row background color

--- js/extras.js
+++ js/extras.js
@@ -3,12 +3,26 @@ $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
 
+// Show merged models when checkbox is checked
 var input = document.getElementById('merged-models-checkbox')
 if (input) {
   var filterMergedModels = function() {
-    var mergedModels = document.getElementsByClassName('merged-model');
-    for (var i = 0; i < mergedModels.length; i++) {
-        mergedModels[i].style.display = input.checked ? 'table-row' : 'none';
+    var table = document.getElementsByTagName('tbody')[0];
+    var rows = table.getElementsByTagName('tr');
+    var bgColours = ['#ffffff', '#f8f8f8'];
+    var bgColour = bgColours[0];
+    for (var i = 0; i < rows.length; i++) {
+      if (input.checked) {
+        rows[i].style.display = 'table-row';
+        rows[i].style.backgroundColor = bgColour;
+      } else {
+        if (rows[i].classList.contains('merged-model')) {
+          rows[i].style.display = input.checked ? 'table-row' : 'none';
+        } else {
+          rows[i].style.display = 'table-row';
+          rows[i].style.backgroundColor = bgColour;
+      }
+      bgColour = bgColours[bgColours.indexOf(bgColour) ^ 1];
     }
   }
   input.addEventListener('change', filterMergedModels)
