da605de897b83bdaeddd715c957ea1f94d1bb78a
saattrupdan <saattrupdan@gmail.com>
2024-02-05 17:28:01 +0100

    fix: Bgcolor

--- js/extras.js
+++ js/extras.js
@@ -9,18 +9,18 @@ if (input) {
   var filterMergedModels = function() {
     var table = document.getElementsByTagName('tbody')[0];
     var rows = table.getElementsByTagName('tr');
-    var bgColours = ['#ffffff', '#f8f8f8'];
+    var bgColours = ['#ffffff', 'rgb(228,228,228)'];
     var bgColour = bgColours[0];
     for (var i = 0; i < rows.length; i++) {
       if (input.checked) {
         rows[i].style.display = 'table-row';
-        // rows[i].style.backgroundColor = bgColour;
+        rows[i].style.backgroundColor = bgColour;
       } else {
         if (rows[i].classList.contains('merged-model')) {
           rows[i].style.display = 'none';
         } else {
           rows[i].style.display = 'table-row';
-          // rows[i].style.backgroundColor = bgColour;
+          rows[i].style.backgroundColor = bgColour;
         }
       }
       bgColour = bgColours[bgColours.indexOf(bgColour) ^ 1];
