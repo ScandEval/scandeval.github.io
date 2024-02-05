33f55250044ea6b240aeca4495f9c582f4f31939
saattrupdan <saattrupdan@gmail.com>
2024-02-05 17:32:45 +0100

    fix: Separate bgcolor and merge filtering

--- js/extras.js
+++ js/extras.js
@@ -3,27 +3,31 @@ $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
 
+
+// Set the background colors of the rows in the leaderboards
+var table = document.getElementsByTagName('tbody')[0];
+if (table) {
+  var rows = table.getElementsByTagName('tr');
+  var bgColours = ['rgb(255,255,255)', 'rgb(228,228,228)'];
+  var bgColour = bgColours[0];
+  var mergedColor = 'rgb(255,194,194)';
+  for (var i = 0; i < rows.length; i++) {
+    if (rows[i].classList.contains('merged-model')) {
+      rows[i].style.backgroundColor = mergedColor;
+    } else {
+      rows[i].style.backgroundColor = bgColour;
+      bgColour = bgColours[bgColours.indexOf(bgColour) ^ 1];
+    }
+  }
+}
+
 // Show merged models when checkbox is checked
 var input = document.getElementById('merged-models-checkbox')
 if (input) {
   var filterMergedModels = function() {
-    var table = document.getElementsByTagName('tbody')[0];
-    var rows = table.getElementsByTagName('tr');
-    var bgColours = ['#ffffff', 'rgb(228,228,228)'];
-    var bgColour = bgColours[0];
-    for (var i = 0; i < rows.length; i++) {
-      if (input.checked) {
-        rows[i].style.display = 'table-row';
-        rows[i].style.backgroundColor = bgColour;
-      } else {
-        if (rows[i].classList.contains('merged-model')) {
-          rows[i].style.display = 'none';
-        } else {
-          rows[i].style.display = 'table-row';
-          rows[i].style.backgroundColor = bgColour;
-        }
-      }
-      bgColour = bgColours[bgColours.indexOf(bgColour) ^ 1];
+    var mergedModels = document.getElementsByClassName('merged-model');
+    for (var i = 0; i < mergedModels.length; i++) {
+      mergedModels[i].style.display = input.checked ? 'table-row' : 'none';
     }
   }
   input.addEventListener('change', filterMergedModels)
