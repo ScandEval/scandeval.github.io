00bd5c6a70140faa621179aca3d30ff49acf750c
saattrupdan <saattrupdan@gmail.com>
2024-02-06 19:43:19 +0100

    feat: Try without special colour for merges

--- js/extras.js
+++ js/extras.js
@@ -9,14 +9,14 @@ function updateBackgroundColors() {
     var rows = table.getElementsByTagName('tr');
     var bgColours = ['rgb(255,255,255)', 'rgb(228,228,228)'];
     var bgColour = bgColours[0];
-    var mergedColor = 'rgba(255,194,194,0.4)';
+    // var mergedColor = 'rgba(255,194,194,0.4)';
     for (var i = 0; i < rows.length; i++) {
-      if (rows[i].classList.contains('merged-model')) {
-        rows[i].style.backgroundColor = mergedColor;
-      } else {
-        rows[i].style.backgroundColor = bgColour;
-        bgColour = bgColours[bgColours.indexOf(bgColour) ^ 1];
-      }
+      // if (rows[i].classList.contains('merged-model')) {
+      //   rows[i].style.backgroundColor = mergedColor;
+      // } else {
+      rows[i].style.backgroundColor = bgColour;
+      bgColour = bgColours[bgColours.indexOf(bgColour) ^ 1];
+      // }
     }
   }
 }
--- js/sortable.js
+++ js/sortable.js
@@ -63,14 +63,14 @@ document.addEventListener('click', function (e) {
       var rows = table.getElementsByTagName('tr');
       var bgColours = ['rgb(255,255,255)', 'rgb(228,228,228)'];
       var bgColour = bgColours[0];
-      var mergedColor = 'rgba(255,194,194,0.4)';
+      // var mergedColor = 'rgba(255,194,194,0.4)';
       for (var i = 0; i < rows.length; i++) {
-        if (rows[i].classList.contains('merged-model')) {
-          rows[i].style.backgroundColor = mergedColor;
-        } else {
-          rows[i].style.backgroundColor = bgColour;
-          bgColour = bgColours[bgColours.indexOf(bgColour) ^ 1];
-        }
+        // if (rows[i].classList.contains('merged-model')) {
+        //   rows[i].style.backgroundColor = mergedColor;
+        // } else {
+        rows[i].style.backgroundColor = bgColour;
+        bgColour = bgColours[bgColours.indexOf(bgColour) ^ 1];
+        // }
       }
     }
   }
@@ -137,7 +137,7 @@ document.addEventListener('click', function (e) {
         // And finally insert the end result
         table.replaceChild(clone_tbody, org_tbody)
 
-        // updateBackgroundColors()
+        updateBackgroundColors()
       }
     } catch (error) {
       // console.log(error)
