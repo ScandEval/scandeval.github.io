718b344ed894f6fd5fb5ae0192939ecc8584faf0
saattrupdan <saattrupdan@gmail.com>
2024-02-06 19:14:16 +0100

    refactor: Assign a click instead of including redundant code

--- js/extras.js
+++ js/extras.js
@@ -3,24 +3,6 @@ $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
 
-
-// Set the background colors of the rows in the leaderboards
-var table = document.getElementsByTagName('tbody')[0];
-if (table) {
-  var rows = table.getElementsByTagName('tr');
-  var bgColours = ['rgb(255,255,255)', 'rgb(228,228,228)'];
-  var bgColour = bgColours[0];
-  var mergedColor = 'rgba(255,194,194,0.4)';
-  for (var i = 0; i < rows.length; i++) {
-    if (rows[i].classList.contains('merged-model')) {
-      rows[i].style.backgroundColor = mergedColor;
-    } else {
-      rows[i].style.backgroundColor = bgColour;
-      bgColour = bgColours[bgColours.indexOf(bgColour) ^ 1];
-    }
-  }
-}
-
 // Show merged models when checkbox is checked
 var input = document.getElementById('merged-models-checkbox')
 if (input) {
@@ -32,4 +14,16 @@ if (input) {
   }
   input.addEventListener('change', filterMergedModels)
   filterMergedModels()
+}
+
+// Sort the language model benchmark by the Score column
+scoreColumn = document.getElementById('score-col-benchmark')
+if(scoreColumn){
+  var click = new MouseEvent("click", {
+      view: window,
+      bubbles: true,
+      cancelable: true,
+      clientX: 20,
+  });
+  scoreColumn.dispatchEvent(click);
 }
